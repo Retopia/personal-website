939138689290e060dc4addfa3cb3d9fb4e23f728
Preston Tang <prestonleetang0123@gmail.com>
2025-02-09 19:11:57 -0500

    Added analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from "@vercel/analytics/react"
 import './index.css'
 import App from './App.jsx'
 
@@ -7,4 +8,5 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
+  <Analytics />
 )
