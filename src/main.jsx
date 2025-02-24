e538ed61a62a1b6473ce507c9929eba2f3a1687a
Preston Tang <prestonleetang0123@gmail.com>
2025-02-24 13:15:21 -0500

    Fixed analytics bugs

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Analytics } from "@vercel/analytics/react"
 import './index.css'
 import App from './App.jsx'
 
@@ -8,5 +7,4 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-  <Analytics />
 )
